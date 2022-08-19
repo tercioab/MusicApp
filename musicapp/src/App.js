80086de34d3a998aa43de9f8ac4042a7b3518aa9
walthercio Almeida <suaagenciainicial@gmail.com>
2022-08-18 21:39:15 -0300

    adiciona Header

---
+++ musicapp/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './components/Header';
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+        <Header />
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
